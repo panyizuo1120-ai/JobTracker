a4d5a1793c3cce024cdccab96157274180f105ae
Yizuo Pan <panyizuo1120@gmail.com>
2026-06-21 17:30:49 +1000

    create main structure

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hellow")
+print("""
+============================
+===========Welcome !========
+==== Job Tracker System ====
+============================
+1. Add Application
+2. Veiw Application
+3. Exit
+============================
+"""
+)
